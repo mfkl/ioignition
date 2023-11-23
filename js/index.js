4217cd9371176fb99eb368f923f39965e94653b2
Cijin Cherian <cijin@seagin.me>
2023-11-23 11:37:30 +0700

    add session time in the payload (#8)

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 "use strict";
 
 const localstoreId = 'ioEventId'
+let startTime
 
 function generateEventId() {
   const id = 'io-' + new Date().getTime() + '-' + Math.random().toString(36).substring(2, 9);
@@ -33,13 +34,16 @@ function sendEvent(eventName, data) {
   }
 
   const eventId = getEventId()
+  const sessionTime = Date.now() - startTime
+  
   const payload = {
     n: eventName,
     u: data.url,
     d: data.domain,
     r: data.referrer,
     w: data.deviceWidth,
-    a: data.userAgent
+    a: data.userAgent,
+    t: sessionTime
   };
 
   const req = new XMLHttpRequest();
@@ -73,12 +77,18 @@ function trackPageview (eventData) {
   trackEvent('pageview', eventData);
 };
 
+function startTimeListener() {
+  startTime = Date.now()
+}
+
 function enableTracking() {
-  window.addEventListener('DOMContentLoaded', trackPageview)
+  window.addEventListener('DOMContentLoaded', startTimeListener)
+  window.addEventListener('beforeunload', trackPageview)
 }
 
 function cleanup() {
-  window.removeEventListener('DOMContentLoaded', trackPageview)
+  window.removeEventListener('DOMContentLoaded', startTimeListener)
+  window.removeEventListener('beforeunload', trackPageview)
 }
 
 enableTracking()
